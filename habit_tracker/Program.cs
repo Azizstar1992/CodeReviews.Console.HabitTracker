fed2d174a276267999e4e75e3adc6eea8745cbba
Azizstar1992 <mohamudabdiaziz7@gmail.com>
2025-11-13 16:39:54 +0000

    finish

---
+++ habit_tracker/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using Microsoft.Data.Sqlite;
+
+namespace habit_tracker{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =(@"Data Source=habit_tracker.db");
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                
+                tableCmd = @
+
+
+
+            }
+        }
+    }
+}
